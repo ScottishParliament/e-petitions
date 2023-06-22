568f8de1ac9df072014a1ae64e949fddd187488f
Andrew White <andrew.white@unboxed.co>
2023-06-23 00:21:27 +0100

    Use replace jasmine gem with jasmine-browser-runner
    The gem has been deprecated for some time and needs to be replaced for Rails 7.0.

--- app/assets/javascripts/frontend/character-counter.js
+++ app/assets/javascripts/frontend/character-counter.js
@@ -12,23 +12,33 @@ document.addEventListener('DOMContentLoaded', function(event) {
     counter.classList.toggle('too-many-characters', charsRemaining < 0);
   }
 
-  var textareas = document.querySelectorAll('textarea[data-max-length]');
+  var attachCounter = function() {
+    var textareas = document.querySelectorAll('textarea[data-max-length]');
 
-  for (var i = 0; i < textareas.length; i++) {
-    var textarea = textareas[i],
-        ariaId = 'char-count-' + textarea.id,
-        counter = textarea.nextElementSibling;
+    for (var i = 0; i < textareas.length; i++) {
+      var textarea = textareas[i],
+          ariaId = 'char-count-' + textarea.id,
+          counter = textarea.nextElementSibling;
 
-    counter.setAttribute('role', 'status');
-    counter.setAttribute('aria-live', 'polite');
-    counter.setAttribute('aria-relevant', 'text');
-    counter.setAttribute('id', ariaId);
-    textarea.setAttribute('aria-controls', ariaId);
+      counter.setAttribute('role', 'status');
+      counter.setAttribute('aria-live', 'polite');
+      counter.setAttribute('aria-relevant', 'text');
+      counter.setAttribute('id', ariaId);
+      textarea.setAttribute('aria-controls', ariaId);
 
-    updateCounter.apply(textarea);
+      updateCounter.apply(textarea);
 
-    textarea.addEventListener('change', updateCounter);
-    textarea.addEventListener('keyup', updateCounter);
-    textarea.addEventListener('paste', updateCounter);
+      textarea.addEventListener('change', updateCounter);
+      textarea.addEventListener('keyup', updateCounter);
+      textarea.addEventListener('paste', updateCounter);
+    }
   }
+
+  var characterCounter = function() {
+    attachCounter();
+  }
+
+  window.GOVUK = window.GOVUK || {};
+  window.GOVUK.PETS = window.GOVUK.PETS || {};
+  GOVUK.PETS.characterCounter = characterCounter;
 });
--- spec/javascripts/character_counter_spec.js
+++ spec/javascripts/character_counter_spec.js
@@ -1,103 +1,102 @@
-//= require jquery
-//= require modules/character-counter
-
 describe('Character counter', function() {
-
-  var $ = window.jQuery,
-      $textbox,
+  var $textbox,
       $counter;
 
   beforeEach(function () {
-    $textbox = $('<textarea data-max-length="50" id="fixture"></textarea>');
-    $counter = $('<p class="character-count">50 characters max</p>');
-    $(document.body).append($textbox);
-    $(document.body).append($counter);
+    $textbox = document.createElement('textarea');
+    $textbox.id = 'fixture';
+    $textbox.dataset.maxLength = '50';
+
+    $counter = document.createElement('p');
+    $counter.className = 'character-count';
+    $counter.textContent = '50 characters max';
+
+    document.body.appendChild($textbox);
+    document.body.appendChild($counter);
   });
 
   afterEach(function () {
-    $textbox.remove();
-    $counter.remove();
+    document.body.removeChild($textbox);
+    document.body.removeChild($counter);
   });
 
   describe("When called", function () {
     it("Puts the max characters into the character counter", function () {
       GOVUK.PETS.characterCounter();
-      expect($counter.text()).toEqual("50");
+      expect($counter.textContent).toEqual('50');
     });
 
     it("Adds an id to the character counter based on the textbox id", function () {
       GOVUK.PETS.characterCounter();
-      expect($counter.attr('id')).toEqual('char-count-' + $textbox.attr('id'));
+      expect($counter.id).toEqual('char-count-' + $textbox.id);
     });
 
     it("Adds an aria-controls attribute to the textbox linking it to the character counter", function () {
       GOVUK.PETS.characterCounter();
-      expect($textbox.attr('aria-controls')).toEqual($counter.attr('id'));
+      expect($textbox.attributes['aria-controls'].value).toEqual($counter.id);
     });
 
     it("Sets the count to what's in the data-max-length attribute on the textbox", function () {
-      $textbox.attr('data-max-length', '40');
+      $textbox.dataset.maxLength = '40';
       GOVUK.PETS.characterCounter();
-      expect($counter.text()).toEqual('40');
+      expect($counter.textContent).toEqual('40');
     });
 
     it("Gives the correct character count for a textbox that has content when the page loads", function () {
-      $textbox.val('Words entered');
+      $textbox.value = 'Words entered';
       GOVUK.PETS.characterCounter();
-      expect($counter.text()).toEqual('37');
+      expect($counter.textContent).toEqual('37');
     });
   });
 
   describe("When content is added to the textbox", function () {
     it("Has the correct character count if some characters are entered by keyup event", function () {
       GOVUK.PETS.characterCounter();
-      $textbox.val('Word entered');
-      $textbox.trigger('keyup');
-      expect($counter.text()).toEqual('38');
+      $textbox.value = 'Word entered';
+      $textbox.dispatchEvent(new KeyboardEvent('keyup'));
+      expect($counter.textContent).toEqual('38');
     });
 
     it("Has the correct character count if some words are entered by paste event", function () {
       GOVUK.PETS.characterCounter();
-      $textbox.val('Words entered');
-      $textbox.trigger('paste');
-      expect($counter.text()).toEqual('37');
+      $textbox.value = 'Words entered';
+      $textbox.dispatchEvent(new ClipboardEvent('paste'));
+      expect($counter.textContent).toEqual('37');
     });
 
     it("Has the correct character count if some words are entered by change event", function () {
       GOVUK.PETS.characterCounter();
-      $textbox.val('Words entered');
-      $textbox.trigger('change');
-      expect($counter.text()).toEqual('37');
+      $textbox.value = 'Words entered';
+      $textbox.dispatchEvent(new Event('change'));
+      expect($counter.textContent).toEqual('37');
     });
 
     it("Has the correct character count if a single character is entered", function () {
       GOVUK.PETS.characterCounter();
-      $textbox.val('w');
-      $textbox.trigger('keyup');
-      expect($counter.text()).toEqual('49');
+      $textbox.value = 'w';
+      $textbox.dispatchEvent(new KeyboardEvent('keyup'));
+      expect($counter.textContent).toEqual('49');
     });
 
     it("Has the correct character count if 50 characters are entered", function () {
       GOVUK.PETS.characterCounter();
-      $textbox.val("Vestibulum vel eleifend nunc. Aliquam fermentum nu");
-      $textbox.trigger('keyup');
-      expect($counter.text()).toEqual('0');
+      $textbox.value = 'Vestibulum vel eleifend nunc. Aliquam fermentum nu';
+      $textbox.dispatchEvent(new KeyboardEvent('keyup'));
+      expect($counter.textContent).toEqual('0');
     });
 
     it("Has the correct character count if 51 characters are entered", function () {
       GOVUK.PETS.characterCounter();
-      $textbox.val("Vestibulum vel eleifend nunc. Aliquam fermentum num");
-      $textbox.trigger('keyup');
-      expect($counter.text()).toEqual('-1');
+      $textbox.value = 'Vestibulum vel eleifend nunc. Aliquam fermentum num';
+      $textbox.dispatchEvent(new KeyboardEvent('keyup'));
+      expect($counter.textContent).toEqual('-1');
     });
 
     it("Has the correct character count if 52 characters are entered", function () {
       GOVUK.PETS.characterCounter();
-      $textbox.val("Vestibulum vel eleifend nunc. Aliquam fermentum numb");
-      $textbox.trigger('keyup');
-      expect($counter.text()).toEqual('-2');
+      $textbox.value = 'Vestibulum vel eleifend nunc. Aliquam fermentum numb';
+      $textbox.dispatchEvent(new KeyboardEvent('keyup'));
+      expect($counter.textContent).toEqual('-2');
     });
   });
-
 });
-
